c6d7a7a70c9fd0ff42b8e89f7eb6ee59e968a437
Jon Nilsson <jon.h.nilsson@gmail.com>
2017-06-13 20:59:24 +0200

    Lint it out

--- lib/index.jsx
+++ lib/index.jsx
@@ -3,59 +3,50 @@ import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
 class DrawableCanvas extends React.Component {
-  static propTypes = {
-    brushColor: PropTypes.string,
-    lineWidth: PropTypes.number,
-    canvasStyle: PropTypes.shape({
-      backgroundColor: PropTypes.string,
-      cursor: PropTypes.string
-    }),
-    clear: PropTypes.bool
-  };
 
+  componentDidMount(){
+    const canvas = ReactDOM.findDOMNode(this);
+
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
+
+    const context = canvas.getContext('2d');
+
+    this.setState({
+      canvas,
+      context
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.clear){
+      this.resetCanvas();
+    }
+  }
   static getDefaultStyle() {
     return {
       backgroundColor: '#FFFFFF',
       cursor: 'pointer'
-    }
-  };
+    };
+  }
+
   static isMobile(){
     return (
       /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
     );
   }
 
-  componentDidMount(){
-    let canvas = ReactDOM.findDOMNode(this);
-
-    canvas.style.width = '100%';
-    canvas.style.height = '100%';
-    canvas.width  = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-
-    let ctx = canvas.getContext('2d');
-
-    this.setState({
-      canvas: canvas,
-      context: ctx
-    });
-  }
-  componentWillReceiveProps(nextProps) {
-    if(nextProps.clear){
-      this.resetCanvas();
-    }
-  }
-
   handleOnMouseDown(e){
-    let rect = this.state.canvas.getBoundingClientRect();
+    const rect = this.state.canvas.getBoundingClientRect();
     this.state.context.beginPath();
     if(DrawableCanvas.isMobile()){
       this.setState({
         lastX: e.targetTouches[0].pageX - rect.left,
         lastY: e.targetTouches[0].pageY - rect.top
       });
-    }
-    else{
+    } else{
       this.setState({
         lastX: e.clientX - rect.left,
         lastY: e.clientY - rect.top
@@ -70,16 +61,15 @@ class DrawableCanvas extends React.Component {
   handleOnMouseMove(e){
 
     if(this.state.drawing){
-      let rect = this.state.canvas.getBoundingClientRect();
-      let lastX = this.state.lastX;
-      let lastY = this.state.lastY;
+      const rect = this.state.canvas.getBoundingClientRect();
+      const lastX = this.state.lastX;
+      const lastY = this.state.lastY;
       let currentX;
       let currentY;
       if(DrawableCanvas.isMobile()){
-        currentX =  e.targetTouches[0].pageX - rect.left;
+        currentX = e.targetTouches[0].pageX - rect.left;
         currentY = e.targetTouches[0].pageY - rect.top;
-      }
-      else{
+      } else{
         currentX = e.clientX - rect.left;
         currentY = e.clientY - rect.top;
       }
@@ -100,22 +90,24 @@ class DrawableCanvas extends React.Component {
   }
 
   draw(lX, lY, cX, cY) {
-    this.state.context.strokeStyle = this.props.brushColor;
-    this.state.context.lineWidth = this.props.lineWidth;
-    this.state.context.moveTo(lX,lY);
-    this.state.context.lineTo(cX,cY);
+    this.setState({
+      strokeStyle: this.props.brushColor,
+      lineWidth: this.props.lineWidth
+    });
+    this.state.context.moveTo(lX, lY);
+    this.state.context.lineTo(cX, cY);
     this.state.context.stroke();
   }
 
   resetCanvas(){
-    let width = this.state.context.canvas.width;
-    let height = this.state.context.canvas.height;
+    const width = this.state.context.canvas.width;
+    const height = this.state.context.canvas.height;
     this.state.context.clearRect(0, 0, width, height);
   }
 
   canvasStyle(){
-    let defaults = DrawableCanvas.getDefaultStyle();
-    let custom = this.props.canvasStyle;
+    const defaults = DrawableCanvas.getDefaultStyle();
+    const custom = this.props.canvasStyle;
     return Object.assign({}, defaults, custom);
   }
 
@@ -134,5 +126,15 @@ class DrawableCanvas extends React.Component {
   }
 
 }
+
+DrawableCanvas.propTypes = {
+  brushColor: PropTypes.string,
+  lineWidth: PropTypes.number,
+  canvasStyle: PropTypes.shape({
+    backgroundColor: PropTypes.string,
+    cursor: PropTypes.string
+  }),
+  clear: PropTypes.bool
+};
 
 export default DrawableCanvas;
